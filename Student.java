3d888e4bbe5c6625ae8a070f0696b30c0e36c469
junaid139 <junaidfardeen8@gmail.com>
2025-12-14 12:21:11 +0530

    add jojo.java file

---
+++ Student.java
@@ -0,0 +1,20 @@
+public class Student {
+
+    String name;
+    int age;
+    double marks;
+
+    public Student(String name, int age, double marks) {
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+
+    void verification() {
+
+        System.out.println("its a student named: " + name);
+    }
+    
+
+    
+}
---
+++ jojo.java
@@ -0,0 +1,35 @@
+
+
+
+public class jojo {
+
+    public static void main(String[] args) {
+
+      Student[] students = new Student[2];
+
+      students[0] = new Student("fardeen", 19, 9.0);
+
+      students[1] = new Student("Junaid", 20, 7.5);
+
+      for (int i = 0; i < students.length; i++) {
+
+        students[i].verification();
+
+        
+      }
+
+
+
+
+       
+ 
+
+
+       
+
+
+    }
+    
+   
+}
+
